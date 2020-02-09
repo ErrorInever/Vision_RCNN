ff70932f8b78085483b16c50abe5a2a1baf981da
Sergey Malk <errorinever@gmail.com>
2020-02-09 10:13:16 +0300

    fix variable

--- detection/utils.py
+++ detection/utils.py
@@ -20,9 +20,10 @@ def collate_fn(batch):
 
 def get_classes():
     """ return dictionary of classes"""
-    PATH = '../config/classes/coco-labels-2014_2017.txt'
-    with open(PATH) as f:
+    path = '../config/classes/coco-labels-2014_2017.txt'
+    num_classes = 81
+    with open(path) as f:
         classes = f.read().splitlines()
-        classes = dict(zip([x for x in range(1, 81)], classes))
+        classes = dict(zip([x for x in range(1, num_classes)], classes))
     return classes
 
